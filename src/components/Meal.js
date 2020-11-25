d48104fe51fc52aa2a4a781a69443659bcb3eb42
devludaley <daleyluis@gmail.com>
2020-11-25 13:35:46 -0500

    add mealInfo && conditional to bypass --cant find url/name/etc of undefined-- bug

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,33 +1,52 @@
 import React, { Fragment } from 'react'
 import {Redirect} from 'react-router-dom'
+import UpdateMealForm from '../forms/UpdateMealForm'
 // import NewMealRecipeForm from '../components/NewMealRecipeForm'
 
 const Meal = (props) => {
+    console.log('Meal -> props.meals1', props.meals);
     // console.log('props here =>',props.meal.id);
     // console.log(`props`, props.id);
     // let meal = props.meals[props.match.params.id -1]
+    console.log('Meal -> props1', props);
+    // console.log('Meal -> props1', meals)
     if (props.meals) {
+    console.log('Meal -> props.meals2', props.meals);
 
         var meal = props.meals.filter(meal => meal.id == props.match.params.id)[0]
+        // [0]
+        
+        // console.log('Meal -> props.match.params.id)[0]', props.match.params.id[0])
+        
+        // console.log('Meal -> props.match.params.id', props.match.params.id);
+        
+        // console.log('Meal -> meal.id', meal.id);
+        var mealInfo =( 
+                <ul style={{listStyleType: 'none'}}>
+                    Category: <li>{meal.category}</li>
+                    Url: <li>{meal.url}</li>
+                    Image Url:<li>{meal.image_url}</li>
+                    Cooking Time<li>{meal.cooking_time} minutes </li>
+                </ul>
+            )
     }
 
+
+
+
     // console.log(`CONSOLED PROPS from MEAL.JS`, props.meals);
-    return( 
+    return(
         <Fragment>
-        <li className='meal-li' key={props.meal} style={{listStyleType: 'none'}}>
+        <UpdateMealForm/>
+        <li className='meal-component' key={props.meal} style={{listStyleType: 'none'}}>
         {/* {meal ? null : <Redirect to='/meals'/>} */}
 
             <h1>
-            {/* {meal.name} */}
             {meal ? meal.name : null}
-
             </h1>
-            <p>
-            {meal.category}
-            {meal.url}
-            {meal.image_url}
-            {meal.cooking_time}
-            </p>
+            {mealInfo ? mealInfo : null} 
+            {/* {meal.name} */}
+            
             <h3 className='recipes-list'>Recipes List
             {/* <button>
                 {meal.recipes ? meal.recipes.length : null}
