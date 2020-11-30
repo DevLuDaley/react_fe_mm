04d2292447ed4a324a8cb3010f50b96ba6ec95a2
devludaley <daleyluis@gmail.com>
2020-11-30 16:40:54 -0500

    refactor MEAL return ternary statement

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import {Redirect} from 'react-router-dom'
 import UpdateMealForm from '../forms/UpdateMealForm'
+
 // import NewMealRecipeForm from '../components/NewMealRecipeForm'
 
 const Meal = (props) => {
@@ -17,50 +18,59 @@ const Meal = (props) => {
         // [0]
         
         // console.log('Meal -> props.match.params.id)[0]', props.match.params.id[0])
-        
+        // var mealID = {meal.id}
         // console.log('Meal -> props.match.params.id', props.match.params.id);
         
         // console.log('Meal -> meal.id', meal.id);
-        var mealInfo =( 
-                <ul style={{listStyleType: 'none'}}>
-                    Category: <li>{meal.category}</li>
-                    Url: <li>{meal.url}</li>
-                    Image Url:<li>{meal.image_url}</li>
-                    Cooking Time<li>{meal.cooking_time} minutes </li>
-                </ul>
-            )
+        // var mealInfo =( 
+        //         <ul contentEditable="true" style={{listStyleType: 'none'}}>
+        //             ID: <li>{meal.id}</li>
+        //             Category: <li>{meal.category}</li>
+        //             Url: <li>{meal.url}</li>
+        //             Image Url:<li>{meal.image_url}</li>
+        //             Cooking Time<li>{meal.cooking_time} minutes </li>
+        //         <button onClick={handleEdit}>Edit</button>
+        //         </ul>
+        //     )
     }
 
+var handleEdit = () => {
+    console.log("HANDLING THE EDIT LU")
+    // console.log('🚀 ~ file: Meal.js ~ line 73 ~ Meal ~ mealInfo.props', mealInfo.props.children);
+}
 
 
 
     // console.log(`CONSOLED PROPS from MEAL.JS`, props.meals);
     return(
         <Fragment>
-        <UpdateMealForm/>
+        Meal Component
+        Meal
         <li className='meal-component' key={props.meal} style={{listStyleType: 'none'}}>
         {/* {meal ? null : <Redirect to='/meals'/>} */}
-
-            <h1>
-            {meal ? meal.name : null}
-            </h1>
-            {mealInfo ? mealInfo : null} 
-            {/* {meal.name} */}
+            {meal ?
+                <div>
+                    <h1> {meal.name} 
+                    </h1>
+                        <ul style={{listStyleType: 'none' }} contentEditable="true">
+                                ID: <li>{meal.id}</li>
+                                Category: <li>{meal.category}</li>
+                                Url: <li>{meal.url}</li>
+                                Image Url:<li>{meal.image_url}</li>
+                                Cooking Time<li>{meal.cooking_time} minutes </li>
+                        </ul>
+                        <button onClick={handleEdit}>Edit</button>
             
-            <h3 className='recipes-list'>Recipes List
-            {/* <button>
-                {meal.recipes ? meal.recipes.length : null}
-            </button> */}
-            </h3>
-            
-            {/* {meal ? meal.recipes.map(recipe => <h5 key={recipe.id}>{recipe.name} Category: {recipe.category} Url: {recipe.url}      </h5>) : null
-            } */}
+                    <h3 className='recipes-list'>Recipes List
+                    {/* <button>
+                        {meal.recipes ? meal.recipes.length : null}
+                    </button> */}
+                    </h3>
+                </div>
+            : null
+                }
         </li>
-        
-   {/* <NewMealRecipeForm meal={meal} /> */}
-   {/* id={props.meals[props.match.params.id -1]} */}
-    </Fragment>
-
+        </Fragment>
     )
 }
 export default Meal
