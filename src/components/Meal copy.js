2c3c0ef529db459061194eff14f829e3a1e19b41
devludaley <daleyluis@gmail.com>
2020-12-02 20:34:59 -0500

    + mapStateToProps && useSelector

---
+++ src/components/Meal copy.js
@@ -0,0 +1,148 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import {Redirect} from 'react-router-dom'
+import { connect} from 'react-redux'
+import UpdateMealForm from '../forms/UpdateMealForm'
+
+// import NewMealRecipeForm from '../components/NewMealRecipeForm'
+
+const Meal = (props) => {
+
+    var [state, setState] = useState([props.meals])
+    var [name, setName] = useState()
+    var [id, setId] = useState(props.id)
+    var [category, setCategory] = useState()
+    var [url, setUrl] = useState()
+
+    useEffect(() => {
+                setName(props.name);
+            }, [props.name]);
+            
+
+    // console.log('Meal -> props.meals1', props.meals);
+    // console.log('props here =>',props.meal.id);
+    // console.log(`props`, props.id);
+    // let meal = props.meals[props.match.params.id -1]
+    // console.log('Meal -> props1', props);
+    // console.log('Meal -> props1', meals)
+    if (props.meals) {
+    console.log('Meal -> props.meals2', props.meals);
+        
+        // var meal = props.meals.filter(meal => meal.id == props.match.params.id)[0]
+        var mealId = parseInt(window.location.href.replace("http://localhost:3001/meals/", ""))
+
+        // var meal = props.meals.filter(meal => meal.id == mealId[0]
+        var meal = props.state.meals.find(meal => meal.id == mealId)
+        
+        // console.log('Meal -> props.match.params.id)[0]', props.match.params.id[0])
+        // var mealID = {meal.id}
+        // console.log('Meal -> props.match.params.id', props.match.params.id);
+        const handleEdit = () => {
+            // `/meals/${id}/edit`
+        }
+        // console.log('Meal -> meal.id', meal.id);
+        var mealInfo =( 
+                        <div>
+                    <h1> {meal.name} 
+                    </h1>
+                        <ul key={meal.id} style={{listStyleType: 'none' }} contentEditable="false">
+                                ID: <li contentEditable="false">{meal.id}</li>
+                                Category: <li>{meal.category}</li>
+                                Url: <li>{meal.url}</li>
+                                Image Url:<li>{meal.image_url}</li>
+                                Cooking Time<li>{meal.cooking_time} minutes </li>
+                        </ul>
+                        <button onClick={handleEdit}>Edit</button>
+
+                    <h3 className='recipes-list'>Recipes List
+                    {/* <button>
+                        {meal.recipes ? meal.recipes.length : null}
+                    </button> */}
+                    </h3>
+                </div>
+            )
+
+        var mealInfo2 =( 
+                        <div>
+                    <h1> {meal.name} 
+                    </h1>
+                        <form key={meal.id}>
+                                ID: <p>{meal.id}</p>
+                                Category: <p>{meal.category}</p>
+                                Url: <p>{meal.url}</p>
+                                Image Url:<p>{meal.image_url}</p>
+                                Cooking Time<p>{meal.cooking_time} minutes </p>
+                        </form>
+                        <button onClick={handleEdit}>Edit</button>
+
+                    <h3 className='recipes-list'>Recipes List
+                    {/* <button>
+                        {meal.recipes ? meal.recipes.length : null}
+                    </button> */}
+                    </h3>
+                </div>
+            )
+
+        var mealInfo1 =( 
+                <div>
+                    <h1> 
+                        {meal.name} 
+                        <button a href={`http://localhost:3001/meals/${id}/edit`}>Edit Meal</button>
+                    </h1>
+                    <ul id='update-meal-form' onSubmit={console.log("SUBMITTED")}>
+                        ID: <p>{meal.id}</p>
+                        Category: <p>{meal.category}</p>
+                        Url: <p>{meal.url}</p>
+                        Image Url:<p>{meal.image_url}</p>
+                        Cooking Time<p>{meal.cooking_time} minutes </p>
+                    </ul>
+                    <h3 className='recipes-list'>Recipes List
+                    {/* <button>
+                        {meal.recipes ? meal.recipes.length : null}
+                    </button> */}
+                    </h3>
+                </div>
+            )
+    }
+
+    
+
+var handleEdit = () => {
+    console.log("HANDLING THE EDIT LU")
+    // console.log('🚀 ~ file: Meal.js ~ line 73 ~ Meal ~ mealInfo.props', mealInfo.props.children);
+}
+
+
+
+    // console.log(`CONSOLED PROPS from MEAL.JS`, props.meals);
+    return(
+        <Fragment>
+        { mealId = parseInt(window.location.href.replace("http://localhost:3001/meals/", ""))},
+        
+        { meal = props.meals.find(meal => meal.id == mealId)}
+
+            <li className='meal-component' key={props.meal} style={{listStyleType: 'none'}}>
+                {/* {meal ? null : <Redirect to='/meals'/>} */} 
+                {
+                    props.meals ? mealInfo1 : <h1>No Meal Loaded. Check api connection.</h1>
+                }
+            </li>
+        </Fragment>
+    )
+}
+    const mapStateToProps = state => {
+        // const { meals } = state;
+        // const meals = state.meals
+        // var mealId = parseInt(window.location.href.replace("http://localhost:3001/meals/", ""))
+        // const meal = meals.find(meal => meal.id == mealId)
+
+
+        return {
+            meals: state.meals
+            // ,
+            // meal: meal
+
+        }
+    }
+
+export default connect(mapStateToProps)(Meal);
+// export default Meal;
--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,129 +1,107 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import {Redirect} from 'react-router-dom'
+import { connect , useSelector } from 'react-redux'
 import UpdateMealForm from '../forms/UpdateMealForm'
 
 // import NewMealRecipeForm from '../components/NewMealRecipeForm'
+// let id = props.match.params.id;
+// const selectCurrentMeal = state => state.meals.find(meal => meal.id == id)
+// const selectMeals = state => state.meals
 
 const Meal = (props) => {
-
-    var [name, setName] = useState()
-    var [id, setId] = useState(props.id)
-    var [category, setCategory] = useState()
-    var [url, setUrl] = useState()
-
-    useEffect(() => {
-                setName(props.name);
-            }, [props.name]);
-            
-
-    // console.log('Meal -> props.meals1', props.meals);
-    // console.log('props here =>',props.meal.id);
-    // console.log(`props`, props.id);
-    // let meal = props.meals[props.match.params.id -1]
-    // console.log('Meal -> props1', props);
-    // console.log('Meal -> props1', meals)
-    if (props.meals) {
-    console.log('Meal -> props.meals2', props.meals);
-
-        // var meal = props.meals.filter(meal => meal.id == props.match.params.id)[0]
-            var mealId = parseInt(window.location.href.replace("http://localhost:3001/meals/", ""))
-
-    // var meal = props.meals.filter(meal => meal.id == mealId
-            var meal = props.meals.find(meal => meal.id == mealId)
-        // [0]
-        
-        // console.log('Meal -> props.match.params.id)[0]', props.match.params.id[0])
-        // var mealID = {meal.id}
-        // console.log('Meal -> props.match.params.id', props.match.params.id);
-        const handleEdit = () => {
-            // `/meals/${id}/edit`
-        }
-        // console.log('Meal -> meal.id', meal.id);
-        var mealInfo =( 
-                        <div>
-                    <h1> {meal.name} 
-                    </h1>
-                        <ul key={meal.id} style={{listStyleType: 'none' }} contentEditable="false">
-                                ID: <li contentEditable="false">{meal.id}</li>
-                                Category: <li>{meal.category}</li>
-                                Url: <li>{meal.url}</li>
-                                Image Url:<li>{meal.image_url}</li>
-                                Cooking Time<li>{meal.cooking_time} minutes </li>
-                        </ul>
-                        <button onClick={handleEdit}>Edit</button>
-
-                    <h3 className='recipes-list'>Recipes List
-                    {/* <button>
-                        {meal.recipes ? meal.recipes.length : null}
-                    </button> */}
-                    </h3>
-                </div>
-            )
-
-        var mealInfo2 =( 
-                        <div>
-                    <h1> {meal.name} 
-                    </h1>
-                        <form key={meal.id}>
-                                ID: <p>{meal.id}</p>
-                                Category: <p>{meal.category}</p>
-                                Url: <p>{meal.url}</p>
-                                Image Url:<p>{meal.image_url}</p>
-                                Cooking Time<p>{meal.cooking_time} minutes </p>
-                        </form>
-                        <button onClick={handleEdit}>Edit</button>
-
-                    <h3 className='recipes-list'>Recipes List
-                    {/* <button>
-                        {meal.recipes ? meal.recipes.length : null}
-                    </button> */}
-                    </h3>
-                </div>
-            )
-
-        var mealInfo1 =( 
-                <div>
-                    <h1> 
-                        {meal.name} 
-                        <button a href={`http://localhost:3001/meals/${id}/edit`}>Edit Meal</button>
-                    </h1>
-                    <ul id='update-meal-form' onSubmit={console.log("SUBMITTED")}>
-                        ID: <p>{meal.id}</p>
-                        Category: <p>{meal.category}</p>
-                        Url: <p>{meal.url}</p>
-                        Image Url:<p>{meal.image_url}</p>
-                        Cooking Time<p>{meal.cooking_time} minutes </p>
-                    </ul>
-                    <h3 className='recipes-list'>Recipes List
-                    {/* <button>
-                        {meal.recipes ? meal.recipes.length : null}
-                    </button> */}
-                    </h3>
-                </div>
-            )
-    }
-
+    // let id = props.match.params.id;
+    // const mealsB = useSelector(selectMeals)
+    // console.log('🚀 ~ file: Meal.js ~ line 14 ~ Meal ~ mealsB', mealsB);
     
+    
+    // const mealsArr = useSelector(state => {[state.meals].find(meal => meal.id == id)})
+    // var mealsArr = useSelector(state => {state.meals.map(meal => meal.name)}) y
+// const mealsArr = useSelector(state => state.meals.find(meal => meal.id == id))
 
-var handleEdit = () => {
-    console.log("HANDLING THE EDIT LU")
-    // console.log('🚀 ~ file: Meal.js ~ line 73 ~ Meal ~ mealInfo.props', mealInfo.props.children);
-}
-
-
-
-    // console.log(`CONSOLED PROPS from MEAL.JS`, props.meals);
+    // const useMeals = useSelector(selectMeals) 
+    // const bigMeal = useMeals.find(meal => meal.id == mealId)
+    // if (props.meals) {
+//     var mealId = props.match.params.id;
+//     var meal = props.meals.find(meal => meal.id === mealId)
+//     // var mealInfo = (<li>{meal.name}</li>)
+//     console.log(props.meals)
+    //  console.log('MEAL WORDS')
+// var soloMeal
+var id;
+if (props.mealsArr) {
+     var showMealCard = (
+         <div>
+       <p>  {id = props.match.params.id} </p>
+       {/* <p>  { props.meals.length } </p> */}
+       {/* <p>  22{ props.meal.name } </p> */}
+         {/* { props.meals[id].name } */}
+         {/* { props.meals[id]} */}
+         {/* <p> { props.meals[id]} </p> */}
+        {console.log('🚀 ~ file: Meal.js ~ line 50 ~ Meal ~ id', id)}
+        {console.log('🚀 ~ file: Meal.js ~ line 47 ~ Meal ~ props.meals', props.meals)}
+        {console.log('🚀 ~ file: Meal.js ~ line 47 ~ Meal ~ props.meals[id]', props.meals[id])}
+        {/* {console.log('🚀 ~ file: Meal.js ~ line 47 ~ Meal ~ props.meal', props.meal)} */}
+         
+          <p>Show Meal Card here 1</p>
+        {/* //  {soloMeal = mealsB.find(meal => meal.id == id)} */}
+        {/* //  <p>{soloMeal.name}</p> */}
+        
+        </div>
+        //  meal = props.meals.find(meal => meal.id == mealId)
+     )
+     }
+//      console.log("MEAL BRO", meal);
+// // var meal = props.meals.find(meal => meal.id == mealId)
+// }
+// var meal
+// console.log(`CONSOLED PROPS from MEAL.JS`, props.meals);
+// var mealId;
+// var meal;
+// var meals;
+// var soloMeal;
     return(
-        <Fragment>
-            Meal Component
-            Meal
-            <li className='meal-component' key={props.meal} style={{listStyleType: 'none'}}>
-                {/* {meal ? null : <Redirect to='/meals'/>} */} 
-                {
-                    meal ? mealInfo1 : <h1>No Meal Loaded. Check api connection.</h1>
-                }
+    <Fragment>
+          {props.mealsArr ? <div>{showMealCard}</div> : <p>NOT A CHANCE</p>}
+          <div> 
+          <p>Show Meal Card here 2</p>
+         {/* {soloMeal = mealsB.find(meal => meal.id == id)} */}
+         {/* <p>{soloMeal.name}</p> */}
+         </div>
+    {/* // var mealInfo = (<li>{meal.name}</li>)
+    // console.log(props.meals)
+    //  console.log("MEAL BRO", meal);
+// var meal = props.meals.find(meal => meal.id == mealId)
+        //   {let id = ownProps.match.params.id} */}
+            <li className='meal-component' style={{listStyleType: 'none'}}>
+            {/* {props.meals ? mealCard:(<p> NADA </p>)} */}
             </li>
+            {console.log("MEAL PROPS", props)}
+            {/* {console.log("MEAL PROPS", props.match.params.id)} */}
+            {/* {console.log("MEAL from PROPS", props.meal)} */}
         </Fragment>
     )
 }
-export default Meal
+// }
+    const mapStateToProps = (state, ownProps) => {
+    // const mapStateToProps = (state) => {
+        const { meals } = state;
+        let mealId = ownProps.match.params.id;
+        return {
+            mealId: mealId,
+            // meal: state.meals,
+            mealsArr: meals
+            // meal: state.meals[mealId]
+            // meal: mealsArr[mealId]
+            // meal: meals.find(meal => meal.id == mealId)
+        }
+    }
+    // const mapDispatchToProps = (dispatch) => {
+
+    // }
+        // const meals = state.meals
+        // var mealId = parseInt(window.location.href.replace("http://localhost:3001/meals/", ""))
+        // const meal = meals.find(meal => meal.id == mealId)
+            // meal: state.meals.find(meal => meal.id === id)
+            // meal: meals.find(meal => meal.id == mealId)
+    // export default Meal;
+    export default connect(mapStateToProps)(Meal);
