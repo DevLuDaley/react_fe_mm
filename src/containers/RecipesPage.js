96d3c85606ac4f2764b2179b7ca9b2d98bdc8688
devludaley <daleyluis@gmail.com>
2020-12-16 04:33:05 -0500

    Create RecipesPage.js

---
+++ src/containers/RecipesPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+// import fetchRecipes from '../actions/fetchRecipes'
+import RecipesList from '../components/RecipesList';
+// import NewRecipeForm from '../forms/NewRecipeForm';
+
+
+class RecipesPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = []
+    }
+
+
+    //   componentDidMount(){
+    //     // this.props.fetchRecipes()
+    // }
+    render() { 
+        return (<>
+        <p> Recipes Page</p>
+        {/* <NewRecipeForm recipes={this.props.recipes}/> */}
+        <RecipesList recipes={this.props.recipes}/>
+        {console.log('🚀 ~ file: RecipesPage.js ~ line 22 ~ RecipesPage ~ render ~ this.props.recipes', this.props.recipes)}
+         </>  );
+    }
+}
+ 
+export default RecipesPage;
