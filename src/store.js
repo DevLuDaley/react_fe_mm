f8dd0c3858bb54f2bab5eaf7bcc97c63ce34728a
devludaley <daleyluis@gmail.com>
2020-12-15 22:32:47 -0500

    Create store.js

---
+++ src/store.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+
+ import mealsReducer from './reducers/mealsReducer'
+ import recipesReducer from './reducers/recipesReducer'
+//  import reportWebVitals from './reportWebVitals';
+
+
+// const reducer = combineReducers({
+//     mealReducer,
+//     recipeReducer
+// })
+
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// + const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+export default store
+
+// reportWebVitals();
