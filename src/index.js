3f66b791c7adc24e94b4a8aa04e13607ad316f2f
devludaley <daleyluis@gmail.com>
2020-12-16 04:33:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import MealList from './components/MealList'
+import MealList from './components/MealsList'
 import reportWebVitals from './reportWebVitals';
 import mealsReducer from './reducers/mealsReducer'
 import recipesReducer from './reducers/recipesReducer'
@@ -21,12 +22,23 @@ import initialRootState from './reducers/allReducers'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+
+// const store = createStore(
+//     rootReducer,
+//     compose(
+//         applyMiddleware(thunk),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//     )
+// );
+
+
+
 // initialRootState = {meals:[], recipes:[]};
-// const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
 // const store = createStore(rootReducer, initialRootState, composeEnhancer(applyMiddleware(thunk)))
 // const store = createStore(allReducers, composeEnhancer(applyMiddleware(thunk)))
-let store = createStore(mealsReducer, composeEnhancer(applyMiddleware(thunk)))
-// const store2 = createStore(recipesReducer, composeEnhancers(applyMiddleware(thunk)))
+// let store = createStore(mealsReducer, composeEnhancer(applyMiddleware(thunk)))
+// const store = createStore(recipesReducer, composeEnhancer(applyMiddleware(thunk)))
 // let store2 = createStore([mealsReducer, recipesReducer], composeEnhancers(applyMiddleware(thunk)))
 // const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
@@ -61,3 +73,4 @@ reportWebVitals();
 // export App = () => {
     // ONLY NOW does it make sense to try to access the store/dispatch actions
 // }
+// export default store;
