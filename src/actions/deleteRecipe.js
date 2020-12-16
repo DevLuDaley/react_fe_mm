5e8c5fe09819eebb03fbb18dde9127d5ea89dbb5
devludaley <daleyluis@gmail.com>
2020-12-16 04:33:31 -0500

    Create deleteRecipe.js

---
+++ src/actions/deleteRecipe.js
@@ -0,0 +1,17 @@
+export const deleteRecipe = (recipeData) => {
+    const baseUrl = 'http://localhost:3000/api/v1/recipes/'
+    const deleteRecipeUrl = (baseUrl + recipeData.id)
+    // console.log('🚀 ~ file: deleteRecipe.js ~ line 3 ~ deleteRecipe ~ baseUrl', baseUrl);
+    // console.log('🚀 ~ file: deleteRecipe.js ~ line 5 ~ deleteRecipe ~ recipeUrl', recipeUrl);
+    // console.log('🚀 ~ file: deleteRecipe.js ~ line 7 ~ deleteRecipe ~ data.id', recipeData.id);
+    // console.log('🚀 ~ file: deleteRecipe.js ~ line 8 ~ return ~ data', recipeData);
+    return (dispatch) => {
+        fetch(deleteRecipeUrl, { 
+                method: 'DELETE'
+    })
+        .then(response => response.json())
+        .then(recipeId => dispatch({type: 'DELETE_MEAL', payload: recipeId}))
+    }
+};
+
+export default deleteRecipe;
