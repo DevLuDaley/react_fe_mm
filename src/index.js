a5a140256e09dad2ae6e55e2de59fbf164a3767a
devludaley <daleyluis@gmail.com>
2020-12-17 20:02:14 -0500

    add subscribe log calls to troubleshoot store/combineReducers bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
 
+import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -13,6 +15,9 @@ import mealsReducer from './reducers/mealsReducer'
 import recipesReducer from './reducers/recipesReducer'
 import rootReducer from './reducers/reducers'
 import initialRootState from './reducers/reducers'
+import MainContainer from './containers/MainContainer'
+import MealsContainer from './containers/MealsContainer'
+import RecipesContainer from './containers/RecipesContainer'
 
 // const rootReducer = combineReducers({
 //     mealsReducer,
@@ -21,8 +26,14 @@ import initialRootState from './reducers/reducers'
 
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+// const myLogger = (store) => (next) => (action) => {
+// console.log('🚀 ~ file: index.js ~ line 25 ~ myLogger ~ Logged Action: ', action);
+  
+// }
 const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+// const store = createStore(combineReducers({mealsReducer, recipesReducer}), {},(applyMiddleware(thunk)))
+
+store.subscribe(() => console.log("store updated", store.getState()))
 
 // const store = createStore(
 //     rootReducer,
@@ -42,7 +53,7 @@ const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
 // let store2 = createStore([mealsReducer, recipesReducer], composeEnhancers(applyMiddleware(thunk)))
 // const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
-console.log('🚀 ~ file: index.js ~ line 21 ~ store.getState()', store.getState());
+console.log('🚀 ~ file: index.js ~ line 56 ~ store.getState()', store.getState());
 
 
 
@@ -62,7 +73,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
 
 // store.dispatch({
 //   type: 'FETCH_RECIPES',
