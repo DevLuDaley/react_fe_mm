f3db97726f3e3435a946ea4fd1084842802eef62
devludaley <daleyluis@gmail.com>
2020-12-17 22:02:46 -0500

    import and use configureStore function from store/configureStore

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { render } from 'react-dom'
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import { configureStore } from '@reduxjs/toolkit';
+// import { configureStore } from '@reduxjs/toolkit';
+import configureStore from './store/configureStore';
 
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import MealList from './components/MealsList'
 import reportWebVitals from './reportWebVitals';
 import mealsReducer from './reducers/mealsReducer'
 import recipesReducer from './reducers/recipesReducer'
-import rootReducer from './reducers/reducers'
-import initialRootState from './reducers/reducers'
-import MainContainer from './containers/MainContainer'
-import MealsContainer from './containers/MealsContainer'
-import RecipesContainer from './containers/RecipesContainer'
+import rootReducer from './reducers/reducer'
+import initialRootState from './reducers/reducer'
+// import MainContainer from './containers/MainContainer'
+// import MealsContainer from './containers/MealsContainer'
+// import RecipesContainer from './containers/RecipesContainer'
 
 // const rootReducer = combineReducers({
 //     mealsReducer,
@@ -25,15 +26,16 @@ import RecipesContainer from './containers/RecipesContainer'
 // })
 
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const myLogger = (store) => (next) => (action) => {
 // console.log('🚀 ~ file: index.js ~ line 25 ~ myLogger ~ Logged Action: ', action);
   
 // }
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+// const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+const store = configureStore()
 // const store = createStore(combineReducers({mealsReducer, recipesReducer}), {},(applyMiddleware(thunk)))
 
-store.subscribe(() => console.log("store updated", store.getState()))
+// store.subscribe(() => console.log("store updated", store.getState()))
 
 // const store = createStore(
 //     rootReducer,
@@ -53,7 +55,7 @@ store.subscribe(() => console.log("store updated", store.getState()))
 // let store2 = createStore([mealsReducer, recipesReducer], composeEnhancers(applyMiddleware(thunk)))
 // const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
-console.log('🚀 ~ file: index.js ~ line 56 ~ store.getState()', store.getState());
+// console.log('🚀 ~ file: index.js ~ line 56 ~ store.getState()', store.getState());
 
 
 
@@ -61,7 +63,8 @@ console.log('🚀 ~ file: index.js ~ line 56 ~ store.getState()', store.getState
 //   <Router><React.StrictMode><App /></React.StrictMode></Router>,
 //   document.getElementById('root')
 // );
-ReactDOM.render(
+render(
+// ReactDOM.render(
   <Provider store={store}>
   <Router><React.StrictMode>
   <App/>
