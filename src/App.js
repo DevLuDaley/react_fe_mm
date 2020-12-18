b9307b504bb5029dbada79a2ae2e293a2829fcd8
devludaley <daleyluis@gmail.com>
2020-12-17 19:58:51 -0500

    src/App.js  => convert to class comp - add mapStateToProps
    create and add MainContainer
    remove NavBar and Footer
    add NavBar and Footer to MainContainer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 // import logo from './logo.svg';
 import './App.css';
-
 import MainContainer from './containers/MainContainer'
+import mealsReducer from './reducers/mealsReducer'
+import recipesReducer from './reducers/recipesReducer'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+// import Header from './components/Header'
 // import MealsContainer from './containers/MealsContainer'
 // import RecipesContainer from './containers/RecipesContainer'
-import Navbar from './components/Navbar'
-// import Header from './components/Header'
-import Footer from './components/Footer'
-
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { meals:[], recipes:[] }
+  }
+  render() { 
+    // return (  );
+  // }
+// }
+// export default App;
+// function App() {
+  console.log('🚀 ~ file: App.js ~ line 29 ~ App ~ this.props.meals', this.props.meals)
   return (
     <div className="App">
         {/* <Header/> */}
-        <Navbar/>
-        <MainContainer/>
+        {/* <Navbar/> */}
+        {/* <Navbar/> */}
+        <MainContainer meals={this.props.meals} recipes={this.props.recipes} className='main-container'/>
         {/* <MealsContainer/> */}
         {/* <RecipesContainer/> */}
-        <Footer/>
+        {/* <Footer id="footer"/> */}
     </div>
   );
 }
+}
 
-export default App;
+const mapStateToProps = (state) => {
+console.log('🚀 ~ file: App.js ~ line 38 ~ mapStateToProps ~ state', state);
+console.log('🚀 ~ file: App.js ~ line 38 ~ mapStateToProps ~ state.mealsReducer.meals', state.mealsReducer.meals);
+  return {
+    meals: state.mealsReducer.meals,
+    recipes: state.recipesReducer.recipes
+  }
+}
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     meals: state.mealsReducer,
+//     recipes: state.recipesReducer
+//   }
+// }
+export default connect(mapStateToProps,null)(App);
