4abd9faad7f13d4ce4e9dfcc9df7205cca7742d6
devludaley <daleyluis@gmail.com>
2020-12-17 20:03:21 -0500

    add configureStore to troubleshoot store/combineReducers bug

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,13 @@
 import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import mealsReducer from './mealsReducer'
 import recipesReducer from './recipesReducer'
+
+// export default configureStore({
+//     reducer:{
+//     mealsReducer,
+//     recipesReducer}
+// });
 
 export default combineReducers({
     mealsReducer,
